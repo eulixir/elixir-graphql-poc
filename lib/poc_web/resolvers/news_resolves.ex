1d162ef7461d54bff779c6406f39ff7db6d574f7
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-06 09:38:53 -0300

    chore: add resolver:

--- lib/poc_web/resolvers/news_resolves.ex
+++ lib/poc_web/resolvers/news_resolves.ex
@@ -4,4 +4,11 @@ defmodule PocWeb.NewsResolver do
   def all_links(_root, _args, _info) do
     {:ok, News.list_links()}
   end
+
+  def create_link(_root, args, _info) do
+    case News.create_link(args) do
+      {:ok, link} -> {:ok, link}
+      _error -> {:error, "could not create link"}
+    end
+  end
 end
